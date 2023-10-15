a89811baa3596017674d7db649468b84bb6599fe
taiyou <hiromacha1116@icloud.com>
2023-10-15 12:59:09 +0900

    API飛び出し成功

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    fetch('http://127.0.0.1:8080/')
+      .then(response => response.text())
+      .then(data => {
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <p>うんち</p>
+      <h1>Hello React!</h1>
     </div>
   );
 }
